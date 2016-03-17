10de37f1f5703715c9bb4d3577bb441ae0207d30
Olli Antikainen <olli.antikainen@helsinki.fi>
2016-03-17 14:03:25 +0200

    update

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-
 $otsikko = 'DJ-playlist database 0.1';
+?><!doctype HTML>
 
-?>
-<!doctype HTML>
 <html>
+
+    <head><title><?php echo ?otsikko; ?></title></head>
     <body>
-        
         <h1><?php echo otsikko; ?></h1>
-        
-        <p>
-            Hei php-maailma!
-        </p>
-    </body>
-    
+        <p> Hei php-maailma! </p>
+
+</body>
 </html>
